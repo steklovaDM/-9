ec97c84ce3ddb6455a88f1f1ee3f7c885d5ca79a
steklovaDM <108463196+steklovaDM@users.noreply.github.com>
2022-08-24 13:50:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+/*
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
+*/
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+/*
+int n, m, s=0;
+Console.Write("Введи значение от: ");
+n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи значение до: ");
+m = Convert.ToInt16(Console.ReadLine());
+for (int i = n; i <= m; i++)
+    s += i;
+Console.WriteLine(s);
+*/
+
